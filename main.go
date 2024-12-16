1efd4e90bf977b61ec93e9560e858bd97edd186f
zhxu <zheng2580369@gmail.com>
2024-12-16 13:17:38 +0800

    init

---
+++ main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fsnotify/fsnotify"
+	"log"
+)
+
+func main() {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	done := make(chan bool)
+	go func() {
+		for {
+			select {
+			case event, ok := <-watcher.Events:
+				if !ok {
+					return
+				}
+				fmt.Println("event:", event)
+				if event.Op&fsnotify.Write == fsnotify.Write {
+					fmt.Println("modified file:", event.Name)
+				}
+			case err, ok := <-watcher.Errors:
+				if !ok {
+					return
+				}
+				fmt.Println("error:", err)
+			}
+		}
+	}()
+
+	err = watcher.Add("C:\\Users\\zhxu\\Pictures\\s")
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-done
+}
